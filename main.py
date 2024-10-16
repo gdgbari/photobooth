22003cb024c89bd3764aef3fbbee814d67f8b773
Gassi Giuseppe <g.gassi@studenti.poliba.it>
2024-10-16 22:28:13 +0200

    main first commit ( it needs in the future a cleare definition, maybe a runner class )

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
+from SettingsManager import Settings
+from FolderManager import FolderManager
+from CameraManager import PhotoManager
+from UserInteraction import UserInterface
+from PhotoTailor import Tailor
+
+import utils
 
 def main():
     print('hello world')
+
+    settings = Settings()
+    folders = FolderManager(settings.get_main_folder_path())
+    user_interface = UserInterface()
+
+    camera = PhotoManager()
+    editor = Tailor()
+
+    # CAMERA
+    # end
+
+    # here the photo is in the 'current' folder
+    # let's ask the user it is the photo he likes
+    photo_path, name = utils.get_the_file_in_dir(folders.get_current_path())
+    user_interface.confirm_shot(photo_path)
+    effect_name = user_interface.choose_polaroid_effect()
+    effect_path = utils.get_asset_path_from_name(effect_name)
+    # let's edit it
+    editor.set_infos(photo_path, effect_path, folders.get_output_folder_path())
+    editor.edit(name)
+
+
 
 
 if __name__ == '__main__':
