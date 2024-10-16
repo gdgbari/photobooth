ad61130988599c0b4bc1987285ada44145786c72
Gassi Giuseppe <g.gassi@studenti.poliba.it>
2024-10-16 18:40:16 +0200

    tailor class finished

--- PhotoTailor.py
+++ PhotoTailor.py
@@ -1,7 +1,7 @@
 # this class get the chosen photo and applies the wanted corner
 
 from PIL import Image
-
+import os
 
 class Tailor:
 
@@ -10,18 +10,47 @@ class Tailor:
         self._effect_path = chosen_effect_path
         self._output_folder_path = output_folder_path
 
-    def edit(self, file_name: str):
-        file_path = self._output_folder_path + "\\" + file_name
+    def edit(self, edited_file_name: str):
+
+        # analysis on the true height of the polaroid 41+760+10 = 810 -> the height of the background (760) is 93%
+        # true height of the polaroid is 2000 -> the height wanted of the background is 1860
+
+        # what about the margin? the left and right margin are of 41 px as the upper margin, 41 px is 5% of height
+        # which with our dimension translate to 100 px
+
+        # the data extrapolated before are from wrong measure
+        # it seems that the correct h of the input image is 1528 px or 76,4 %
+        # it seam that the correct upper margin is 83 px or 4,15 %
+
+        file_path = os.path.join(self._output_folder_path,edited_file_name)
         background = Image.open(self._photo_path)
         foreground = Image.open(self._effect_path)
 
+        # transform the background in order to have a height of 1528 px
+        background_width, background_height = background.size
+        new_background_height = 1528
+        new_background_width = int((new_background_height / background_height) * background_width)
+        background = background.resize((new_background_width, new_background_height), Image.Resampling.LANCZOS)
+
+        # align the background width to the foreground width
+        fg_width, fg_height = foreground.size
+        margin_to_cut = (new_background_width - fg_width) // 2
+        background = background.crop((margin_to_cut, 0, margin_to_cut + fg_width, new_background_height))
+
+        # add space on the top and the bottom of the background image
+        margin_space = 82
+        new_background = Image.new('RGB',(fg_width,fg_height),'black')
+        new_background.paste(background,(0, margin_space))
+        background = new_background
+
+        # combine background and foreground
         background.paste(foreground,(0,0),foreground)
 
         background.save(file_path, "PNG")
 
 # DEBUG
-ph_path = r"C:\Users\User\OneDrive\Desktop\PhotosOFBooth\current\test.jpg"
-eff_path = r"C:\Users\User\PycharmProjects\pythonProject\PhotoBooth\Assets\Polaroid - 1.png"
-output_f = r"C:\Users\User\OneDrive\Desktop\PhotosOFBooth\output"
-tailor = Tailor(ph_path,eff_path,output_f)
-tailor.edit("prova.png")
+# ph_path = "/home/gape01/PycharmProjects/photobooth/Assets/test.jpg"
+# eff_path = "/home/gape01/PycharmProjects/photobooth/Assets/Polaroid - 1.png"
+# output_f = "/home/gape01/Desktop"
+# tailor = Tailor(ph_path,eff_path,output_f)
+# tailor.edit("prova.png")
