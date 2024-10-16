dbc9a18bdb22b19741dad2f84a489fdf3ea435d1
Gassi Giuseppe <g.gassi@studenti.poliba.it>
2024-10-16 19:41:10 +0200

    folder manager finished

--- FolderManager.py
+++ FolderManager.py
@@ -1,3 +1,4 @@
+import os
 # FOLDERS EXPLANATION
 #
 #    main_folder
@@ -12,13 +13,17 @@ class FolderManager:
 
         self._main_folder_path = main_folder_path
         # now the paths of all the sub-folders
-        self._current_folder_path = main_folder_path + '\\current'
-        self._originals_folder_path = main_folder_path + '\\originals'
-        self._output_folder_path = main_folder_path + '\\output'
+        self._current_folder_path = os.path.join(main_folder_path,'current')
+        self._originals_folder_path = os.path.join(main_folder_path,'originals')
+        self._output_folder_path = os.path.join(main_folder_path,'output')
 
         # then we check the folders consistency
         self._folder_consistency_assurance()
 
     def _folder_consistency_assurance(self):
         # if the sub-folders misses, will be created
-        print('yeah ill do it later, it will work')
+        folder_list = [self._main_folder_path, self._current_folder_path,
+                       self._originals_folder_path, self._output_folder_path]
+        for folder in folder_list:
+            if not os.path.exists(folder):
+                os.makedirs(folder)
