af5d09e07ba53ec8660f1292fad169993c2237a3
GassiGiuseppe <g.gassi@studenti.poliba.it>
2024-10-23 16:14:48 +0200

    added the multi printing concept to the queue, queue changed from FILO to FIFO

--- QueueManager.py
+++ QueueManager.py
@@ -11,26 +11,30 @@ class QueueManager:
     def queue_is_ready(self):
         return len(self._dict['photos']) >= 2
 
-    def add_photo(self, photo_path):
-        self._dict['photos'].append(photo_path)
+    def add_photo(self, photo_path, times):
+        for _ in range(times):
+            self._dict['photos'].append(photo_path)
         with open(self._queue_path, 'w') as file:
             yaml.dump(self._dict, file, default_flow_style=False, allow_unicode=True)
 
-    def add_edit(self, edit_path):
-        self._dict['edits'].append(edit_path)
+    def add_edit(self, edit_path, times):
+        for _ in range(times):
+            self._dict['edits'].append(edit_path)
         with open(self._queue_path, 'w') as file:
             yaml.dump(self._dict, file, default_flow_style=False, allow_unicode=True)
 
     def get_two_photos(self)-> list[str]:
-        photos = self._dict['photos'][-2:]  # get last 2 photos from the queue
-        self._dict['photos'] = self._dict['photos'][:-2]  # delete last 2 photos from the queue
+        photos = self._dict['photos'][:2]  # get first 2 photos from the queue
+        # self._dict['photos'] = self._dict['photos'][:-2]  # delete last 2 photos from the queue
+        del self._dict['photos'][:2]
         with open(self._queue_path, 'w') as file:
             yaml.dump(self._dict, file, default_flow_style=False, allow_unicode=True)
         return photos
 
     def get_two_edit(self) -> list[str]:
-        edits = self._dict['edits'][-2:]
-        self._dict['edits'] = self._dict['edits'][:-2]
+        edits = self._dict['edits'][:2]
+        # self._dict['edits'] = self._dict['edits'][:-2]
+        del self._dict['edits'][:2]
         with open(self._queue_path, 'w') as file:
             yaml.dump(self._dict, file, default_flow_style=False, allow_unicode=True)
         return edits
