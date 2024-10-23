62f693a6341120610134314c77b2c31da3f3d268
GassiGiuseppe <g.gassi@studenti.poliba.it>
2024-10-23 21:44:55 +0200

    the queuemanager now work with the temp_data only within its range ( only with the list of photos and edits )

--- QueueManager.py
+++ QueueManager.py
@@ -2,11 +2,21 @@ from types import NoneType
 
 import yaml
 
+
+
 class QueueManager:
 
     def __init__(self):
         self._queue_path = 'temp_data.yaml'
         self._dict = {'photos':[],'edits':[]}
+
+    def _update_yaml(self):
+        with open(self._queue_path, 'r') as f:
+            yaml_dict = yaml.safe_load(f)
+        yaml_dict['photos'] = self._dict['photos']
+        yaml_dict['edits'] = self._dict['edits']
+        with open(self._queue_path, 'w') as f:
+            yaml.dump(yaml_dict, f, default_flow_style=False, allow_unicode=True)
 
     def queue_is_ready(self):
         return len(self._dict['photos']) >= 2
@@ -14,29 +24,25 @@ class QueueManager:
     def add_photo(self, photo_path, times):
         for _ in range(times):
             self._dict['photos'].append(photo_path)
-        with open(self._queue_path, 'w') as file:
-            yaml.dump(self._dict, file, default_flow_style=False, allow_unicode=True)
+        self._update_yaml()
 
     def add_edit(self, edit_path, times):
         for _ in range(times):
             self._dict['edits'].append(edit_path)
-        with open(self._queue_path, 'w') as file:
-            yaml.dump(self._dict, file, default_flow_style=False, allow_unicode=True)
+        self._update_yaml()
 
     def get_two_photos(self)-> list[str]:
         photos = self._dict['photos'][:2]  # get first 2 photos from the queue
         # self._dict['photos'] = self._dict['photos'][:-2]  # delete last 2 photos from the queue
         del self._dict['photos'][:2]
-        with open(self._queue_path, 'w') as file:
-            yaml.dump(self._dict, file, default_flow_style=False, allow_unicode=True)
+        self._update_yaml()
         return photos
 
     def get_two_edit(self) -> list[str]:
         edits = self._dict['edits'][:2]
         # self._dict['edits'] = self._dict['edits'][:-2]
         del self._dict['edits'][:2]
-        with open(self._queue_path, 'w') as file:
-            yaml.dump(self._dict, file, default_flow_style=False, allow_unicode=True)
+        self._update_yaml()
         return edits
 
     def load_queue(self):
@@ -47,9 +53,8 @@ class QueueManager:
             self._dict['edits'] = yaml_dict['edits']
 
     def dismiss(self):
-        dict = {'photos': [], 'edits': []}
-        with open(self._queue_path, 'w') as file:
-            yaml.dump(dict, file, default_flow_style=False, allow_unicode=True)
+        self._dict = {'photos': [], 'edits': []}
+        self._update_yaml()
 
 
 #DEBUG
