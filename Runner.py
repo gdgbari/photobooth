f54942c28daa8c58b63796494d350c38b65b64a0
Gassi Giuseppe <g.gassi@studenti.poliba.it>
2024-10-24 23:32:40 +0200

    runner now works with the correct camera and shows a preview

--- Runner.py
+++ Runner.py
@@ -20,20 +20,22 @@ class Runner:
         self._file_naming = FileNaming()
 
     def prepare(self):
-        # self._camera.start_camera()
+        self._camera.start_camera()
         self._queue.load_queue()
+
 
     def main_execution(self):
         while True:
-            # self._camera.get_shoot_from_pc(self._folders.get_current_path(), self._file_naming.get_photo_name(), self._ui)
-            self._camera.get_fake_shoot(self._folders.get_current_path(),self._file_naming.get_photo_name() ,self._ui)
+            self._camera.get_shoot_from_pc(self._folders.get_current_path(), self._file_naming.get_photo_name(), self._ui)
+            # self._camera.get_fake_shoot(self._folders.get_current_path(),self._file_naming.get_photo_name() ,self._ui)
             self._file_naming.increment_session_number()
             photo_path, photo_name = utils.get_the_file_in_dir(self._folders.get_current_path())
             if self._ui.confirm_shot(photo_path, utils.detect_os()):
                 # the photo is accepted, we can go on
                 break
-        effect_name = self._ui.choose_polaroid_effect()
-        effect_path = utils.get_asset_path_from_name(effect_name)
+        # effect_name = self._ui.choose_polaroid_effect()
+        # effect_path = utils.get_asset_path_from_name(effect_name)
+        effect_path = self.choice_edit_with_preview(photo_path)
 
         times = self._ui.choose_times_to_print()
         # the photo is added to the queue and the folder get cleared
@@ -42,6 +44,13 @@ class Runner:
 
         while self._queue.queue_is_ready(): # if there are 2 or more photos in queue then start to edit
             self.edit(photo_path, effect_path)
+
+    def choice_edit_with_preview(self, photo_path):
+        while True:
+            effect_name = self._ui.choose_polaroid_effect()
+            effect_path = utils.get_asset_path_from_name(effect_name)
+            if self._ui.show_preview_image(self._editor.prepare_single_photo(photo_path,effect_path)):
+                return effect_path
 
 
     def edit(self, photo_path, effect_path):
