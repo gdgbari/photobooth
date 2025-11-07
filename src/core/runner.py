e63298b45f8c8c3a3e9b0c2b7d32dccee8588030
GassiGiuseppe <g.gassi@studenti.poliba.it>
2025-11-07 17:48:17 +0100

    slimmed down runner

--- src/core/runner.py
+++ src/core/runner.py
@@ -111,36 +111,6 @@ class Runner:
                 shutil.move(os.path.join(self._folders.get_current_path(), file_name), os.path.join(self._folders.get_originals_path(), file_name))
                 return False
 
-    def show_single_edit(self, photo_path):
-        """
-        Method which shows the preview of the edited photo with the single effect and returns the effect path if accepted or False if not.
-        :param photo_path: photo path to edit
-        :return: effect path or False
-        """
-
-        effect_name = self._assets.get_corners_names()[0]+".png"
-        effect_path = utils.get_asset_path_from_name(effect_name)
-        # ugly application to get faster
-        # self._ui.show_preview_without_response(self._editor.prepare_single_photo(photo_path,effect_path))
-        if self._ui.show_preview_image(self._editor.prepare_single_photo(photo_path, effect_path)):
-            return effect_path
-        else:
-            return False
-
-    def choice_edit_with_preview(self, photo_path):
-        '''
-        Method which allows the user to choose the effect to apply to the shooted photo with a preview.
-        Returns the effect path when the edited photo is accepted.
-        :param photo_path: photo path to edit
-        :return: effect path
-        '''
-
-        while True:
-            effect_name = self._ui.choose_polaroid_effect()
-            effect_path = utils.get_asset_path_from_name(effect_name)
-            if self._ui.show_preview_image(self._editor.prepare_single_photo(photo_path, effect_path)):
-                return effect_path
-
 
     def edit(self):
         '''
